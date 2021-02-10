eb8cefc794dcb99c22fc2e982bb051002db958c1
Pratik Chopade <Pratik Chopade@DESKTOP-I7JINC7>
2021-02-10 13:38:26 -0500

    commit first

---
+++ Springprojectdemo2/src/main/java/com/example/demo/Springprojectdemo2Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springprojectdemo2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Springprojectdemo2Application.class, args);
+	}
+
+}
